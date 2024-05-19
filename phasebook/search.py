71e2e480b8962d6cc41aba0fc876e2815ad4409b
aceladines <ajladines01@gmail.com>
2024-05-20 01:36:31 +0800

    [FEATURE] - Added a function that will enable users to search by any given parameters (id, name, age, occupation)

--- phasebook/search.py
+++ phasebook/search.py
@@ -25,6 +25,29 @@ def search_users(args):
         a list of users that match the search parameters
     """
 
-    # Implement search here!
 
-    return USERS
+    # Implement search here!
+    matched_users = set()
+
+    results = [
+        obj for obj in USERS
+          if (
+            (args.get('id') and obj.get('id') == args['id']) or
+            (args.get('name') and args['name'].lower() in obj.get('name', '').lower()) or
+            (args.get('age') and int(args['age']) - 1 <= int(obj.get('age', 0)) <= int(args['age']) + 1) or
+            (args.get('occupation') and args['occupation'].lower() in obj.get('occupation', '').lower())
+        )
+
+        if obj['id'] not in matched_users and not matched_users.add(obj['id'])
+    ]
+
+    results.sort(key=lambda user: (
+        0 if args.get('id') and user.get('id') == args['id'] else 1,
+        0 if args.get('name') and args['name'].lower() in user.get('name', '').lower() else 1,
+        0 if args.get('age') and int(args['age']) - 1 <= int(user.get('age', 0)) <= int(args['age']) + 1 else 1,
+        0 if args.get('occupation') and args['occupation'].lower() in user.get('occupation', '').lower() else 1
+    ))
+
+    return results
+
+
